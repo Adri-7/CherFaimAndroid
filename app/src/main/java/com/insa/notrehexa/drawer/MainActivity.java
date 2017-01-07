2b748f1256ecf34e9e8b3c1adca88cf261336482
nathan-k- <nathan.kienlen@gmail.com>
2017-01-07 18:45:35 +0100

    testing things

--- app/src/main/java/com/insa/notrehexa/drawer/MainActivity.java
+++ app/src/main/java/com/insa/notrehexa/drawer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.insa.notrehexa.drawer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -85,7 +86,8 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
-
+            Intent intent = new Intent(this, activity_amis.class);
+            this.startActivity(intent);
         }  else if (id == R.id.nav_share) {
 
         }
