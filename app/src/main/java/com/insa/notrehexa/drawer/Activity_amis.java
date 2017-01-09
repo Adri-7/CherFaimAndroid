591f2faead26882c5e4c20ed5e67260099e2be2f
nathan-k- <nathan.kienlen@gmail.com>
2017-01-09 10:07:13 +0100

    proper toolbar integration (thx Naima)

--- app/src/main/java/com/insa/notrehexa/drawer/Activity_amis.java
+++ app/src/main/java/com/insa/notrehexa/drawer/Activity_amis.java
@@ -3,18 +3,32 @@ package com.insa.notrehexa.drawer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.support.v7.view.ActionMode;
+
+import android.view.Menu;
+
 
 public class Activity_amis extends AppCompatActivity {
 
+
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amis);
 
-        try {
-            //getSupportActionBar().hide();
-        } catch (Exception ignored) {}
+        // Set the defined toolbar as the action bar
+        toolbar = (Toolbar) findViewById(R.id.toolbar_layout_amis);
+        setSupportActionBar(toolbar);
 
+        // Add Up Action to the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
     }
 }
