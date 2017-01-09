a39fa9edf9992c8a415cc33c2fe3ba9e03bf5237
William Hiver <whiver@users.noreply.github.com>
2017-01-09 22:28:51 +0100

    Use native Settings API tu display prefs. Add switches to display num/mail (#6)

--- app/src/main/java/com/insa/notrehexa/drawer/ParamsActivity.java
+++ app/src/main/java/com/insa/notrehexa/drawer/ParamsActivity.java
@@ -8,21 +8,10 @@ import android.view.Menu;
 
 public class ParamsActivity extends AppCompatActivity {
 
-    Toolbar toolbar;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_params);
-
-        // Set the defined toolbar as the action bar
-        toolbar = (Toolbar) findViewById(R.id.toolbar_layout_params);
-
-        setSupportActionBar(toolbar);
-
-        // Add Up Action to the action bar
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new ParamsFragment()).commit();
     }
 
 }
